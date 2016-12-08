0694f975eac178dce7377a8fd999ae500f93d8a5
LucindaX <ahmed.sa3eed_azrk@hotmail.com>
2016-12-08 15:34:14 +0200

    lib module read gems and server request

--- lib_checker/lib/lib_checker.rb
+++ lib_checker/lib/lib_checker.rb
@@ -1,5 +1,35 @@
 require "lib_checker/version"
+require "bundler"
+require "http"
 
 module LibChecker
-  # Your code goes here...
+	class Connector
+		
+		def initialize
+			@gems = nil
+		end
+		
+		# Collect Gems required in the projects Gemfile
+		def getGems
+			@gems = Bundler.load.specs.map { |spec| spec.name }
+		end
+		
+		# Fetch required systems libraries from server
+		def getPackages
+			# retrieve OS and arch info
+			# lsb_release is a utility installed with all debian new versions
+			# which contains system information
+			lsb = `lsb_release -irc`
+			machine_info = [] # [ Distribution ID , Release , Codename , Arch ]
+			lsb.split("\n").map { |ele| machine_info << ele.split("\t")[1] }
+			# add arch to machine_info
+			machine_info << `arch`.chomp
+			# fetch response from server
+			response = HTTP.post("http://localhost:3000/api/packages", :json => { :gems => @gems, :machine_info => machine_info })
+			return nil if response.code != 200
+			packages = response.parse["packages"]
+			return packages
+		end
+
+	end
 end
